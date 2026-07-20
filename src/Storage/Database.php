ad9e5353f7bbffc2d8eab508c27e74c0b4762b71
manasseh <chegemanasseh2@gmail.com>
2026-07-20 17:21:37 +0300

    Database connection

---
+++ src/Storage/Database.php
@@ -0,0 +1,30 @@
+<?php
+namespace App\Storage;
+
+use PDO;
+
+class Database
+{
+    protected $db;
+    protected $host;
+    protected $database;
+    protected $username;
+    protected $password;
+    protected $dsn;
+    protected $pdo;
+    public function __construct()
+    {
+        $this->db = $_ENV['DB_CONNECTION'];
+        $this->host = $_ENV['DB_HOST'];
+        $this->database = $_ENV['DB_DATABASE'];
+        $this->username = $_ENV['DB_USERNAME'];
+        $this->password = $_ENV['DB_PASSWORD'];
+        $this->dsn = $this->db . ':host=' . $this->host . ';dbname=' . $this->database;
+        $this->pdo = new PDO($this->dsn, $this->username, $this->password);
+    }
+  
+    public function getPDO()
+    {
+        return $this->pdo;
+    }
+}
