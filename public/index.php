c7e0cecdc722ebe054499aa0a26cc99801b21ebe
manasseh <chegemanasseh2@gmail.com>
2026-07-20 17:20:16 +0300

    Routing to the index file

--- public/index.php
+++ public/index.php
@@ -1,4 +1,11 @@
 <?php
 
+
 require_once __DIR__ . '/../vendor/autoload.php';
 
+$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
+$dotenv->safeLoad();
+
+[$router, $request] = require __DIR__ . '/../src/Routes/web.php';
+
+$router->dispatch($request->getMethod(), $request->getUri(), $request);
