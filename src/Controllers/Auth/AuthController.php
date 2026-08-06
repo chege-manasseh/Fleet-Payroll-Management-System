a6bdb026d0da79793cfe3fb2d1c182e94cb46f57
manasseh <chegemanasseh2@gmail.com>
2026-08-07 02:35:35 +0300

    refresh tokens

--- src/Controllers/Auth/AuthController.php
+++ src/Controllers/Auth/AuthController.php
@@ -5,15 +5,18 @@ use App\Controllers\Controller;
 use App\Core\Request;
 use App\Core\Response;
 use App\Services\Auth\AuthService;
+use App\Services\Auth\RefreshToken;
 use App\Storage\Database;
 
 class AuthController extends Controller
 {
     private AuthService $authService;
+    private RefreshToken $refreshTokenService;
 
-    public function __construct(AuthService $authService)
+    public function __construct(AuthService $authService,RefreshToken $refreshTokenService)
     {
         $this->authService = $authService;
+        $this->refreshTokenService=$refreshTokenService;
     }
 
     public function register(Request $request){
@@ -29,19 +32,22 @@ class AuthController extends Controller
     public function changePassword(Request $request){
         return $this->authService->changePassword($request);
     }
-    // public function forgotPassword(Request $request){
-    //     return $this->authService->forgotPassword($request,$this->response);
-    // }
-    // public function resetPassword(Request $request){
-    //     return $this->authService->resetPassword($request,$this->response);
-    // }
-    // public function verifyResetPassword(Request $request){
-    //     return $this->authService->verifyResetPassword($request,$this->response);
-    // }
-    // public function verifyChangePassword(Request $request){
-    //     return $this->authService->verifyChangePassword($request,$this->response);
-    // }
-    // public function verifyForgotPassword(Request $request){
-    //     return $this->authService->verifyForgotPassword($request,$this->response);
-    // }
+    public function forgotPassword(Request $request){
+        return $this->authService->forgotPassword($request);
+    }
+    public function resetPassword(Request $request){
+        return $this->authService->resetPassword($request,$this->response);
+    }
+    public function verifyResetPassword(Request $request){
+        return $this->authService->verifyResetPassword($request,$this->response);
+    }
+    public function verifyChangePassword(Request $request){
+        return $this->authService->verifyChangePassword($request,$this->response);
+    }
+    public function verifyForgotPassword(Request $request){
+        return $this->authService->verifyForgotPassword($request,$this->response);
+    }
+    public function refresh(Request $request){
+        return $this->refreshTokenService->refreshToken($request);
+    }
 }
