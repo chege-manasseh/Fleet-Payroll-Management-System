c926d8dc70b0cc8ee57c8a347da42d3d42961594
manasseh <chegemanasseh2@gmail.com>
2026-08-10 16:09:42 +0300

    Singleton container for Database,Request,Response and Router

--- src/Routes/web.php
+++ src/Routes/web.php
@@ -12,24 +12,24 @@ use App\Services\Auth\RefreshToken;
 
 $container = Container::getInstance();
 
-$container->singleton(Request::class,function(){
+$container->singleton(Request::class, function () {
     return new Request();
 });
 
-$container->singleton(Database::class,function(){
+$container->singleton(Database::class, function () {
     return new Database();
 });
 
 
-$container->singleton(Router::class,function(){
+$container->singleton(Router::class, function () {
     return new Router();
 });
 
-$container->singleton(JwtMiddleware::class,function() use($container){
+$container->singleton(JwtMiddleware::class, function () use ($container) {
     return $container->build(JwtMiddleware::class);
 });
 
-$container->singleton(Response::class,function(){
+$container->singleton(Response::class, function () {
     return new Response();
 });
 
@@ -40,12 +40,12 @@ $jwtMiddleware = $container->make(JwtMiddleware::class);
 
 $router->add('GET', '/', [HomeController::class, 'index']);
 $router->add('POST', '/login', [AuthController::class, 'login']);
-$router->add('GET','/testconnection',[HomeController::class, 'testConnection']);
-$router->add('POST','/change-password',[AuthController::class, 'changePassword']);
-$router->add('POST','/refresh',[RefreshToken::class, 'refreshToken']);
-$router->add('POST','/register',[AuthController::class, 'register']);
-$router->add('POST','/logout',[AuthController::class, 'logout']);
-$router->add('POST','/reset-password',[AuthController::class, 'resetPassword']);
+$router->add('GET', '/testconnection', [HomeController::class, 'testConnection']);
+$router->add('POST', '/change-password', [AuthController::class, 'changePassword']);
+$router->add('POST', '/refresh', [RefreshToken::class, 'refreshToken']);
+$router->add('POST', '/register', [AuthController::class, 'register']);
+$router->add('POST', '/logout', [AuthController::class, 'logout']);
+$router->add('POST', '/reset-password', [AuthController::class, 'resetPassword']);
 
 
 $publicRoutes = [
@@ -59,14 +59,15 @@ $publicRoutes = [
         '/register' => [AuthController::class, 'register'],
         '/logout' => [AuthController::class, 'logout'],
         '/refresh' => [AuthController::class, 'refreshToken'],
-        '/reset-password' => [AuthController::class, 'resetPassword']
+        '/reset-password' => [AuthController::class, 'resetPassword'],
+        '/' => [HomeController::class, 'index']
     ],
 ];
 
-if(isset($publicRoutes[$request->getMethod()]) && isset($publicRoutes[$request->getMethod()][$router->normalizeUri($request->getUri())])){
-    return [$router,$request,$response,$container];
+if (isset($publicRoutes[$request->getMethod()]) && isset($publicRoutes[$request->getMethod()][$router->normalizeUri($request->getUri())])) {
+    return [$router, $request, $response, $container];
 }
 
-if($jwtMiddleware->handle()=== true){
-    return [$router,$request,$response,$container];
+if ($jwtMiddleware->handle() === true) {
+    return [$router, $request, $response, $container];
 }
