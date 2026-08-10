14ca60a931b4457a50728589f0dbb3d8cfee9db5
manasseh <chegemanasseh2@gmail.com>
2026-08-10 16:09:02 +0300

    Refresh token rotation

--- src/Services/Auth/RefreshToken.php
+++ src/Services/Auth/RefreshToken.php
@@ -1,44 +1,82 @@
 <?php
 
 namespace App\Services\Auth;
+
 use App\Controllers\Controller;
 use App\Core\Request;
 use App\Core\Response;
 use App\Models\Users;
 use App\Models\RefreshTokens;
+use App\Storage\Database;
+use Dotenv\Exception\ExceptionInterface;
+use Exception;
 
 class RefreshToken
 {
-    public $message;
-    public $data;
     private Response $response;
     private RefreshTokens $refreshTokens;
+    private Database $db;
+    private AuthService $authService;
+    private Users $users;
 
-    public function __construct( Response $response,RefreshTokens $refreshTokens)
+    public function __construct(Users $users, Database $db, Response $response, RefreshTokens $refreshTokens, AuthService $authService)
     {
-        $this->message = 'Refresh token';
-        $this->data = null;
-        $this->response= $response;
-        $this->refreshTokens=$refreshTokens;
+        $this->response = $response;
+        $this->refreshTokens = $refreshTokens;
+        $this->db = $db;
+        $this->authService = $authService;
+        $this->users = $users;
     }
 
     public function refreshToken(Request $request)
     {
-        
-        $refreshToken = $request->getCookie('refresh_token');
-        if(!$refreshToken){
-            $this->message = 'Refresh Token Invalid';
-            $this->data = null;
-            return $this->response->json($this->message, $this->data, 401);
+        $this->db->beginTransaction();
+        $rawToken = $request->getCookie('refresh_token');
+        if (!$rawToken) {
+            $message = 'Refresh Token Invalid';
+            $data = null;
+            return $this->response->json($message, $data, 401);
         }
-        $refreshToken = hash($_ENV['JWT_ALGORITHM'], $refreshToken);
-        $refreshToken = $this->refreshTokens->getRefreshToken($refreshToken);
-        if(!$refreshToken){
-            $this->message = 'Refresh token not found';
-            $this->data = null;
-            return $this->response->json($this->message, $this->data, 401);
+        $hashToken = hash('sha256', $rawToken);
+        try {
+            $tokenRecord = $this->refreshTokens->getRefreshToken($hashToken);
+
+            if ($tokenRecord === false) {
+                $this->db->rollBack();
+                return $this->response->json('Unauthorized', null, 401);
+            }
+
+            $currentTime = time();
+            if ($tokenRecord['is_revoked'] == false || strtotime($tokenRecord['expires_at']) > $currentTime) {
+                $oldToken = [
+                    'is_revoked' => true,
+                    'revoked_at' => date('Y-m-d H:i:s', $currentTime)
+                ];
+                $this->refreshTokens->updateRefreshToken($oldToken, $hashToken);
+                $user = $this->users->getUser($tokenRecord['user_id']);
+                $tokens = $this->authService->generateToken($tokenRecord['user_id'], $user['role']);
+                $newToken = [
+                    'user_id' => $tokenRecord['user_id'],
+                    'token_hash' => $tokens["refreshToken"],
+                    'expires_at' => $tokens['expiresAt'],
+                    'parent_token_hash' => $hashToken,
+                    'root_token_hash' => $tokenRecord['root_token_hash'] ?? $hashToken
+                ];
+                $this->refreshTokens->saveRefreshToken($newToken);
+                $this->db->commit();
+                return $this->response->json("token refreshed", null, 200);
+            } else {
+                //invalidate all refreshtokens for this user
+                if ($tokenRecord['is_revoked'] == true) {
+                    // Invalidate all tokens for this user family tree
+                    $this->refreshTokens->deleteRefreshToken($tokenRecord['user_id']);
+                }
+                $this->db->commit();
+                $this->response->json("UNAUTHORIZED LOG IN A FRESH", null, 401);
+            }
+        } catch (Exception $e) {
+            $this->db->rollBack();
+            return $this->response->json("Server Error", ['error' => $e->getMessage()], 500);
         }
-        $accessToken = JWT::encode($refreshToken, $_ENV['JWT_SECRET'], $_ENV['JWT_ALGORITHM']);
-        return $this->response->json($this->message, $this->data, 200);
     }
 }
